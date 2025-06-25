f8805f07d124023c185ba6f3cac4d876a7e8e021
Estudiante Ltic <estudiante@sistemas.local>
2025-06-25 17:54:52 -0500

    iTunfy final

--- usuarios/views.py
+++ usuarios/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from .models import Usuario
 
 def index ( request ):
-    return render ( request , 'usuarios / index . html ')
+    return render ( request , 'usuarios/index.html')
 
 def form ( request ):
-    return render ( request , 'usuarios / form . html ')
+    return render ( request , 'usuarios/form.html')
 
 def list ( request ):
-    return render ( request , 'usuarios / list . html ')
+    return render ( request , 'usuarios/list.html')
